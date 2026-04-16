c1c187706c397a03fc57b73db7a3cb322a5f1352
Jasmine Caicedo <jecaiced@uci.edu>
2026-04-16 15:41:02 -0700

    Player now animates with movement

---
+++ My Darling Star (Vertical Slice)/Assets/Script/Dialoge Node.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueLine", menuName = "ScriptableObjects/DialogueLine", order = 1)]
+public class DialogueNode : ScriptableObject
+{
+    public string _NPCName;
+    public string[] _lines;
+    public Sprite characterPortrate;
+    public string[] _playerReplyOptions;
+    public DialogueNode[] _npcReplies;
+    public DialogueNode[] _differentObjectHeldNodes;
+}
